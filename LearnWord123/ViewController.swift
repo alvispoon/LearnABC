5e3cc023ef41ab2e6338b07c2ec6f97a9d52712f
Poon Tsz Kin <alvispoon@gmail.com>
2020-05-26 15:56:55 +0800

    Initial Commit

---
+++ LearnWord123/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnWord123
+//
+//  Created by Alvis Poon on 26/5/2020.
+//  Copyright © 2020 Alvis Poon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
